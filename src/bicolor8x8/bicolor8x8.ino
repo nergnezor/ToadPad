76095952c859440857b217e4740c4ea4e8b81f95
nergnezor <e@roseng.ren>
2023-07-03 14:00:31 +0200

    fix rotation

--- src/bicolor8x8/bicolor8x8.ino
+++ src/bicolor8x8/bicolor8x8.ino
@@ -21,24 +21,30 @@ public:
             return;
         if ((x < 0) || (x >= 8))
             return;
+#ifndef _swap_int16_t
+#define _swap_int16_t(a, b) \
+    {                       \
+        int16_t t = a;      \
+        a = b;              \
+        b = t;              \
+    } ///< 16-bit var swap
+#endif
+        switch (getRotation())
+        {
+        case 1:
+            _swap_int16_t(x, y);
+            x = 8 - x - 1;
+            break;
+        case 2:
+            x = 8 - x - 1;
+            y = 8 - y - 1;
+            break;
+        case 3:
+            _swap_int16_t(x, y);
+            y = 8 - y - 1;
+            break;
+        }
 
-        // switch (getRotation())
-        // {
-        // case 1:
-        //     _swap_int16_t(x, y);
-        //     x = 8 - x - 1;
-        //     break;
-        // case 2:
-        //     x = 8 - x - 1;
-        //     y = 8 - y - 1;
-        //     break;
-        // case 3:
-        //     _swap_int16_t(x, y);
-        //     y = 8 - y - 1;
-        //     break;
-        // }
-
-        // Override drawing to align colors
         if (color == LED_GREEN)
         {
             // x = 8 - x - 1;
@@ -125,6 +131,18 @@ uint8_t keyScanArrayIndex = 0;
 bool keyScanFound = 0;
 static int nKeys;
 
+static const uint8_t PROGMEM smile_bmp[] = {B00111100, B01000010, B10100101,
+                                            B10000001, B10100101, B10011001,
+                                            B01000010, B00111100},
+                             neutral_bmp[] = {B00111100, B01000010, B10100101,
+                                              B10000001, B10111101, B10000001,
+                                              B01000010, B00111100},
+                             frown_bmp[] = {B00111100, B01000010, B10100101,
+                                            B10000001, B10011001, B10100101,
+                                            B01000010, B00111100};
+
+int circle[] = {4, 4, 4};
+
 void setup()
 {
     Serial.begin(115200);
@@ -134,7 +152,7 @@ void setup()
         Wire.setPins(i2cPins[i2cLine].sda, i2cPins[i2cLine].scl);
         for (size_t address = 0; address < 8; address++)
         {
-            Display *key = &matrix[nKeys];
+            Display *key = (Display *)&matrix[nKeys];
             // if (key->begin(0x70 + address) == 0)
             key->begin(0x70 + address);
             {
@@ -153,24 +171,30 @@ void setup()
                 Serial.print(", adress: ");
                 Serial.print(address);
                 Serial.println();
-                key->setRotation(0);
                 key->setRotation(3);
-                if (nKeys % NCols == 0)
+                if (address == 4 && i2cLine == 0 ||
+                    address == 5 && i2cLine == 2 ||
+                    address == 6 && i2cLine == 1 ||
+                    address == 1 && i2cLine == 2)
                 {
-                    // key->setRotation(1);
+                    key->setRotation(1);
                 }
-                key->setBrightness(100);
+                if (address == 1 && i2cLine == 1)
+                {
+                    key->setRotation(2);
+                }
+                key->setBrightness(10);
 
-                key->setTextColor(LED_RED);
-                key->setCursor(1, 1);
-                key->print(address);
-                // key->setTextColor(LED_YELLOW);
-                // key->setCursor(2, 1);
-                // key->print(address);
-                key->setRotation(0);
-                key->setTextColor(LED_GREEN);
+                // key->drawBitmap(0, 0, smile_bmp, 8, 8, LED_GREEN);
+
+                key->setTextColor(LED_YELLOW);
                 key->setCursor(2, 1);
                 key->print(address);
+                // key->setRotation(0);
+                key->setTextColor(LED_GREEN);
+                key->setCursor(1, 1);
+                key->print(address);
+
                 key->writeDisplay();
 
                 if (nKeys >= N_KEYS)
@@ -180,17 +204,6 @@ void setup()
     }
 }
 
-static const uint8_t PROGMEM smile_bmp[] = {B00111100, B01000010, B10100101,
-                                            B10000001, B10100101, B10011001,
-                                            B01000010, B00111100},
-                             neutral_bmp[] = {B00111100, B01000010, B10100101,
-                                              B10000001, B10111101, B10000001,
-                                              B01000010, B00111100},
-                             frown_bmp[] = {B00111100, B01000010, B10100101,
-                                            B10000001, B10011001, B10100101,
-                                            B01000010, B00111100};
-
-int circle[] = {4, 4, 4};
 int brightness;
 void loop()
 {
